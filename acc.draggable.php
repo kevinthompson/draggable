96bf0a7158dd360ef28606422000df488281c499
Kevin Thompson <thompson.kevind@gmail.com>
2010-07-04 08:20:58 -0700

    Initialize Repository

---
+++ acc.draggable.php
@@ -0,0 +1,77 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Draggable_acc {
+
+	var $name			= 'Draggable';
+	var $id				= 'draggable';
+	var $version		= '1.0';
+	var $description	= 'Add drag and drop sorting to various areas of the control panel.';
+	var $sections		= array();
+	var $valid_pages	 = array(
+		'field_management' => array(
+			'table' => 'exp_channel_fields',
+			'field'	=> 'field_order',
+			'id'	=> 'field_id'
+		),
+		'status_management' => array(
+			'table' => 'exp_statuses',
+			'field'	=> 'status_order',
+			'id'	=> 'status_id'
+		)
+	);
+
+	/**
+	 * Constructor
+	 */
+	function Draggable_acc()
+	{
+		$this->EE =& get_instance();
+		
+		if($this->EE->input->get('M') != '' && array_key_exists($this->EE->input->get('M'),$this->valid_pages)){
+			
+			$page = $this->valid_pages[$this->EE->input->get('M')];
+			
+			$this->EE->cp->load_package_js('jquery.tablednd_0_5');
+			$this->EE->cp->load_package_js('jquery.json-2.2.min');
+			$this->EE->cp->load_package_js('draggable');
+			$this->EE->cp->add_to_foot('
+			<script type="text/javascript">
+			//<![CDATA[
+			EE.draggable = {
+				table: "' . $page['table'] . '",
+				field: "' . $page['field'] . '",
+				id:    "' . $page['id'] . '"
+			}
+			//]]>
+			</script>
+			');
+		}
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Set Sections
+	 *
+	 * Set content for the accessory
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	function set_sections()
+	{
+		if($this->EE->input->get('M') != '' && array_key_exists($this->EE->input->get('M'),$this->valid_pages))
+		{
+			$this->sections['Drag and Drop Sorting Enabled'] = '<p style="width:300px;">To reorder table rows on this page, move your mouse cursor over a row (but not over a link within the row), and your cursor will change to a "move" cursor. Click and drag the row to its new position in the table and the updated order will automatically be saved.</p>';
+		}else{
+			$this->sections['<script type="text/javascript">$("#accessoryTabs .' . $this->id  . '").parent("li").hide();</script>'] = "This is not the accessory you're looking for.";
+		}
+	}
+
+	// --------------------------------------------------------------------
+
+}
+// END CLASS
+
+/* End of file acc.draggable.php */
+/* Location: ./system/expressionengine/third_party/accessories/acc.draggable.php */
---
+++ ext.draggable.php
@@ -0,0 +1,89 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Draggable_ext
+{
+	var $settings        = array();
+	var $name            = 'Draggable';
+	var $version         = '1.0';
+	var $description     = 'Add drag and drop sorting to various areas of the control panel.';
+	var $settings_exist  = 'n';
+	var $docs_url		 = '';
+
+	function Draggable_ext($settings='')
+	{
+	    $this->settings = $settings;
+	    $this->EE =& get_instance();
+	}
+	
+	function update_order($session)
+	{
+		if($this->EE->input->post('draggable_ajax') != '')
+		{
+
+			// add json lib if < PHP 5.2
+			include_once 'includes/jsonwrapper/jsonwrapper.php';
+			
+			// decode json data
+			$fields = json_decode($this->EE->input->post('draggable_ajax'));
+			$db = json_decode($this->EE->input->post('draggable_db'));
+			
+			// store new values
+			$sql = "UPDATE " . $db->table . " SET " . $db->field . " = CASE " . $db->id . " ";
+			
+			foreach($fields as $index => $field)
+			{
+				$field = (array) $field;
+				$index += 1;
+				$sql .= "WHEN " . $field[$db->id] . " THEN " . $index . " ";
+				$csv .= ($csv != '' ? ',' : '') . $field[$db->id];
+				$group_id = ($field['group_id'] != '' ? $field['group_id'] : "");
+			}
+			
+			$sql .= "END WHERE " . $db->id . " IN (" . $csv . ")" . ($group_id != '' ? " AND group_id = " . $group_id : "");
+			
+			$this->EE->db->query($sql);
+			
+			// kill ee execution
+			exit();
+		}
+	}
+	
+		
+	function activate_extension()
+	{
+
+	  $this->EE->db->query($this->EE->db->insert_string('exp_extensions',
+	    	array(
+				'extension_id' => '',
+		        'class'        => ucfirst(get_class($this)),
+		        'method'       => 'update_order',
+		        'hook'         => 'sessions_end',
+		        'settings'     => '',
+		        'priority'     => 10,
+		        'version'      => $this->version,
+		        'enabled'      => "y"
+				)
+			)
+		);
+	}
+
+
+	function update_extension($current='')
+	{
+	    if ($current == '' OR $current == $this->version)
+	    {
+	        return FALSE;
+	    }
+	    
+		$this->EE->db->query("UPDATE exp_extensions 
+	     	SET version = '". $this->EE->db->escape_str($this->version)."' 
+	     	WHERE class = '".ucfirst(get_class($this))."'");
+	}
+
+	
+	function disable_extension()
+	{	    
+		$this->EE->db->query("DELETE FROM exp_extensions WHERE class = '".ucfirst(get_class($this))."'");
+	}
+
+}
