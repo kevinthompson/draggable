37e81266747e5d432b8d575098f43e2e29ef296b
Kevin Thompson <thompson.kevind@gmail.com>
2010-09-25 23:12:07 -0700

    Debugging

--- draggable/ext.draggable.php
+++ draggable/ext.draggable.php
@@ -67,7 +67,7 @@ class Draggable_ext
 				
 				//$this->db->where($db->id_field,$field[$db->id_field]);
 				//if($group_id != '') $this->db->where('group_id',$group_id);
-				$this->db->update($db->table,$data);
+				//$this->db->update($db->table,$data);
 			}
 			
 			// Kill EE Execution
